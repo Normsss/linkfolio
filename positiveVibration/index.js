8321cabce8b5936030ba81cecca98cc58c9e1f83
NormaBr <norms.dev@protonmail.com>
2025-01-28 01:15:55 -0600

    positive

---
+++ positiveVibration/index.js
@@ -0,0 +1,6 @@
+// Change the text between the quotation marks and run your code to see the name of your favorite language animated!
+drawText('Positive Vibration', purple);
+
+
+// The code below will allow you to interact with the letters
+bounceBubbles();
